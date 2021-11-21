e3cf57b66d5092e6becf29864850fe16145cab73
BangKiHyun <rlrlvh@naver.com>
2021-11-21 18:05:19 +0900

    Update 다중 파일 읽기 예제

--- definitive-guide/src/main/java/com/javabom/definitiveguide/chap7/job/PatternMatchingJobConfiguration.java
+++ definitive-guide/src/main/java/com/javabom/definitiveguide/chap7/job/PatternMatchingJobConfiguration.java
@@ -1,7 +1,9 @@
 package com.javabom.definitiveguide.chap7.job;
 
+import com.javabom.definitiveguide.chap7.job.reader.CustomerFileReader;
 import com.javabom.definitiveguide.chap7.mapper.TransactionFieldSetMapper;
 import com.javabom.definitiveguide.chap7.model.CustomerAddress;
+import com.javabom.definitiveguide.chap7.model.CustomerTransactions;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -10,7 +12,9 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.MultiResourceItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.builder.MultiResourceItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.FieldSetMapper;
 import org.springframework.batch.item.file.mapping.PatternMatchingCompositeLineMapper;
@@ -37,19 +41,18 @@ public class PatternMatchingJobConfiguration {
     @Bean(name = JOB_NAME)
     public Job job() {
         return this.jobBuilderFactory.get(JOB_NAME)
-                .start(customLineTokenizerFileStep())
+                .start(customMultiFileStep())
                 .build();
     }
 
-    @Bean(name = STEP_NAME)
-    public Step customLineTokenizerFileStep() {
-        return this.stepBuilderFactory.get(STEP_NAME)
+    @Bean(name = "first" + STEP_NAME)
+    public Step customAddressLineTokenizerFileStep() {
+        return this.stepBuilderFactory.get("first" + STEP_NAME)
                 .<CustomerAddress, CustomerAddress>chunk(10)
                 .reader(patternMatchingItemReader(null))
                 .writer(patternMatchingItemWriter())
                 .build();
     }
-
 
     @Bean
     @StepScope
@@ -80,6 +83,84 @@ public class PatternMatchingJobConfiguration {
         lineMapper.setFieldSetMappers(fieldSetMapperMap);
 
         return lineMapper;
+    }
+
+    @Bean(name = "second" + STEP_NAME)
+    public Step customTransactionLineTokenizerFileStep() {
+        return this.stepBuilderFactory.get("second" + STEP_NAME)
+                .<CustomerTransactions, CustomerTransactions>chunk(10)
+                .reader(customerFileReader())
+                .writer(patternMatchingItemWriter())
+                .build();
+    }
+
+    @Bean
+    public CustomerFileReader customerFileReader() {
+        return new CustomerFileReader(customerMultiItemReader());
+    }
+
+    @Bean
+    public PatternMatchingCompositeLineMapper customerTransactionsLineMapper() {
+        Map<String, LineTokenizer> lineTokenizerMap = new HashMap<>(2);
+        lineTokenizerMap.put("CUST*", customerLineTokenizer()); //고객 정보 포맷
+        lineTokenizerMap.put("TRANS*", transactionLineTokenizer()); //거래 레코드 포맷
+
+        Map<String, FieldSetMapper> fieldSetMapperMap = new HashMap<>(2);
+        BeanWrapperFieldSetMapper<CustomerTransactions> customerFieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        customerFieldSetMapper.setTargetType(CustomerTransactions.class);
+
+        fieldSetMapperMap.put("CUST*", customerFieldSetMapper);
+        fieldSetMapperMap.put("TRANS*", new TransactionFieldSetMapper());
+
+        PatternMatchingCompositeLineMapper lineMapper = new PatternMatchingCompositeLineMapper();
+        lineMapper.setTokenizers(lineTokenizerMap);
+        lineMapper.setFieldSetMappers(fieldSetMapperMap);
+
+        return lineMapper;
+    }
+
+    @Bean
+    public FlatFileItemReader customerMultiItemReader() {
+        return new FlatFileItemReaderBuilder<CustomerTransactions>()
+                .name("customerMultiItemReader")
+                .lineMapper(customerTransactionsLineMapper())
+                .build();
+    }
+
+    @Bean(name = "third" + STEP_NAME)
+    public Step customMultiFileStep() {
+        return this.stepBuilderFactory.get("third" + STEP_NAME)
+                .<CustomerTransactions, CustomerTransactions>chunk(10)
+                .reader(customerMultiFileItemReader(null))
+                .writer(patternMatchingItemWriter())
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public MultiResourceItemReader customerMultiFileItemReader(
+            @Value("#{jobParameters['customerFile']}") ClassPathResource[] inputFiles) {
+        return new MultiResourceItemReaderBuilder<>()
+                .name("customerMultiFileItemReader")
+                .resources(inputFiles)
+                .delegate(customerMultiFileReader())
+                .build();
+    }
+
+    @Bean
+    public CustomerFileReader customerMultiFileReader() {
+        return new CustomerFileReader(customerMultiItemReader());
+    }
+
+    @Bean
+    @StepScope
+    public FlatFileItemReader customerTransactionsItemReader(
+            @Value("#{jobParameters['customerFile']}") ClassPathResource inputFile) {
+        return new FlatFileItemReaderBuilder<CustomerTransactions>()
+                .name("customerTransactionsItemReader")
+                .lineMapper(customerTransactionsLineMapper())
+                .resource(inputFile)
+                .build();
     }
 
     @Bean
--- definitive-guide/src/test/java/com/javabom/definitiveguide/chap7/job/PatternMatchingJobConfigurationTests.java
+++ definitive-guide/src/test/java/com/javabom/definitiveguide/chap7/job/PatternMatchingJobConfigurationTests.java
@@ -4,11 +4,11 @@ import com.javabom.definitiveguide.test.BatchSpringTest;
 import com.javabom.definitiveguide.test.TestJobLauncher;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.batch.core.BatchStatus;
-import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.*;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,6 +28,46 @@ class PatternMatchingJobConfigurationTests {
 
         //when
         final JobExecution execution = jobLauncher.launchJob(PatternMatchingJobConfiguration.JOB_NAME, jobParameters);
+        final List<StepExecution> stepExecutions = new ArrayList<>(execution.getStepExecutions());
+
+        //then
+        assertThat(execution.getStatus()).isEqualTo(BatchStatus.COMPLETED);
+        assertThat(stepExecutions.get(0).getReadCount()).isEqualTo(9);
+        assertThat(stepExecutions.get(0).getWriteCount()).isEqualTo(9);
+    }
+
+    @DisplayName("ItemStreamReader 인터페이스 구현을 통한 여러 레코드 포맷 파일 읽기")
+    @Test
+    public void customerFileItemReaderTest() throws Exception {
+        //given
+        final JobParameters jobParameters = new JobParametersBuilder()
+                .addString("customerFile", "input/customerMultiFormat1.csv")
+                .toJobParameters();
+
+        //when
+        final JobExecution execution = jobLauncher.launchJob(PatternMatchingJobConfiguration.JOB_NAME, jobParameters);
+        final List<StepExecution> stepExecutions = new ArrayList<>(execution.getStepExecutions());
+
+        //then
+        assertThat(execution.getStatus()).isEqualTo(BatchStatus.COMPLETED);
+        assertThat(stepExecutions.get(0).getReadCount()).isEqualTo(3);
+        assertThat(stepExecutions.get(0).getWriteCount()).isEqualTo(3);
+    }
+
+    @DisplayName("여러 파일 읽기")
+    @Test
+    public void multiFileItemReaderTest() throws Exception {
+        //given
+        final JobParameters jobParameters = new JobParametersBuilder()
+                .addString("customerFile",
+                        "input/customerMultiFormat1.csv," +
+                                "input/customerMultiFormat2.csv," +
+                                "input/customerMultiFormat3.csv")
+                .toJobParameters();
+
+        //when
+        final JobExecution execution = jobLauncher.launchJob(PatternMatchingJobConfiguration.JOB_NAME, jobParameters);
+        final List<StepExecution> stepExecutions = new ArrayList<>(execution.getStepExecutions());
 
         //then
         assertThat(execution.getStatus()).isEqualTo(BatchStatus.COMPLETED);
